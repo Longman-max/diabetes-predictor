a226c790fb4cef14fef1ed6d158a74f8645c077b
Longman-max <147675147+Longman-max@users.noreply.github.com>
2025-04-10 22:58:58 +0100

    fixed routing and added styling

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get form data
